dc2187ee180fb534f54550e7f9d9593ae44c69bd
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-19 08:54:06 +0700

    Fix bug in undo button

--- javascript.js
+++ javascript.js
@@ -16,12 +16,28 @@ function mouseoutEffect(){
 
 // Add display function
 const display=document.querySelector(".display");
-
-for(let button of buttons){
-    console.log(button.textContent)
-    button.addEventListener("click", clickToDisplay)
-    
+const numbers=document.querySelectorAll(".num");
+const operators=document.querySelectorAll(".operator")
+for(let number of numbers){
+    number.addEventListener("click", clickToDisplay)
+}
+for(let operator of operators){
+    operator.addEventListener("click", clickToDisplay)
 }
 function clickToDisplay(){
     display.textContent+=this.textContent
+}
+
+// Add clear button function
+const clear=document.querySelector("#clear");
+clear.addEventListener("click", clearDisplay)
+function clearDisplay(){
+    display.textContent="";
+}
+
+// Add undo button function
+const undo=document.querySelector("#undo");
+undo.addEventListener("click", undoDisplay)
+function undoDisplay(){
+    display.textContent=display.textContent.substring(0,display.textContent.length-1)
 }
